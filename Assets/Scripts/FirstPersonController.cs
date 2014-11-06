6eedc1cde71e5ede64a3e7454be1f584510f0656
mccartjj <mccartjj@email.sc.edu>
2014-11-06 00:35:28 -0500

    fresh start, terrain is set up, needs to be detailed. We need inventory and items to pick up (just red blocks for food would be great)

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+
+	public float movementSpeed = 6.0f;
+	public float mouseSens = 3.0f;
+
+	float verticalRotation= 0;
+	public float upDownRange = 60.0f;
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = true;
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		//rotation
+		float rotX = Input.GetAxis ("Mouse X") * mouseSens;
+		transform.Rotate (0, rotX, 0);
+
+		verticalRotation -= Input.GetAxis ("Mouse Y") * mouseSens;
+		verticalRotation = Mathf.Clamp (verticalRotation, -upDownRange, upDownRange);
+		Camera.main.transform.localRotation = Quaternion.Euler (verticalRotation, 0, 0);
+
+		//movement
+		float forwardSpeed = Input.GetAxis ("Vertical") * movementSpeed;
+		float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
+		Vector3 speed = new Vector3(sideSpeed,0, forwardSpeed);
+
+		speed = transform.rotation * speed;
+
+		CharacterController cc = GetComponent <CharacterController>();
+
+		cc.SimpleMove (speed );
+	}
+}
